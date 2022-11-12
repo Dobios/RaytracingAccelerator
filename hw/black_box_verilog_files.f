b19dfbbeb367acf58b4ade6307ad73ce898f9832
Andrew Dobis <andrew.dobis@epfl.ch>
2022-11-12 17:10:22 +0100

    removed build files

--- hw/black_box_verilog_files.f
+++
@@ -1,2 +0,0 @@
-/home/andrewdobis/Desktop/Bachelor_Thesis/Raytracer/lap---raytracing-hardware-accelerator-for-fpga/Raytracer_Chisel/rayIntersect.v
-/home/andrewdobis/Desktop/Bachelor_Thesis/Raytracer/lap---raytracing-hardware-accelerator-for-fpga/Raytracer_Chisel/XilinxSimpleDualPortNoChangeBRAM.v
